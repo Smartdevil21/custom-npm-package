fd09fc20111a240aed1afb6c432a17c80fef63b6
Smartdevil21 <prtk.app.dev@gmail.com>
2024-01-20 19:15:19 +0530

    Update!

--- build/errors/notAuthorized.error.js
+++ build/errors/notAuthorized.error.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NotAuthorizedError = void 0;
 const custom_error_1 = require("./custom-error");
 class NotAuthorizedError extends custom_error_1.CustomError {
+    serializeErrors() {
+        return [{ message: "401 Unauthorized" }];
+    }
     constructor() {
         super("Unauthorized request!");
         this.statusCode = 401;
         Object.setPrototypeOf(this, NotAuthorizedError.prototype);
-    }
-    serializeErrors() {
-        return [{ message: "401 Unauthorized" }];
     }
 }
 exports.NotAuthorizedError = NotAuthorizedError;
