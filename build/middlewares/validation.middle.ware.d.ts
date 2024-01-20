b573cd1de50286c040ed1d8c621364d6fdab530a
Smartdevil21 <prtk.app.dev@gmail.com>
2024-01-20 19:15:53 +0530

    Update!

--- build/middlewares/validation.middle.ware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-declare const reqValidationMiddleware: (req: Request, res: Response, next: NextFunction) => void;
-export { reqValidationMiddleware };
